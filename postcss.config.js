5420a94be6f74b77315c16d1377fb04e793d3e2f
junki-kanda <kanda.junki@gmail.com>
2025-08-01 08:41:19 +0900

    Fix merge conflicts and prepare for production deployment

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-module.exports = {
-=======
 export default {
->>>>>>> da4bff2 (second commit)
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-<<<<<<< HEAD
-}; 
-=======
 }
->>>>>>> da4bff2 (second commit)
